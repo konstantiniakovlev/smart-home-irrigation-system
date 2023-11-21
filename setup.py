02e7d49b6fc7b3cc275fd5d375006a6ae9a755cb
Konstantin Iakovlev <konstantin.iakovlev@gmail.com>
2023-11-21 18:53:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="smart-home-irrigation-system",
+    version="0.1.0",
+    url="",
+    author="konstantiniakovlev",
+    description="",
+    install_requires=[],
+)
